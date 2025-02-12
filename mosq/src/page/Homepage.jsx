99a2916efda72e487312288678d190ba26827dbb
Lanxxxe <Lanxxxe>
2025-02-13 01:40:15 +0800

    minimal adjustments on some spacing fix responsiveness accross multiple devices

--- mosq/src/page/Homepage.jsx
+++ mosq/src/page/Homepage.jsx
@@ -49,10 +49,8 @@ const Homepage = () => {
   const [mosquitoData, setMosquitoData] = useState(null);
   const [mostFrequent, setMostFrequent] = useState(null);
   const [otherMosquitoes, setOtherMosquitoes] = useState([]);
-  // const [data, processData] = useState([]);
 
   useEffect(() => {
-    // Initial data fetch using Axios
     axios.get('http://localhost:3000/api/detected-stats')
       .then(response => {
         processData(response.data.rows);
@@ -61,7 +59,6 @@ const Homepage = () => {
         console.error('Error fetching mosquito data:', error);
       });
 
-      // Connect to WebSocket for real-time updates
       const ws = new WebSocket('ws://localhost:3000');
 
       ws.onopen = () => {
@@ -77,19 +74,15 @@ const Homepage = () => {
         console.log('WebSocket disconnected');
       };
 
-      // Cleanup WebSocket connection on unmount
       return () => {
         ws.close();
       };
   }, []);
 
-  // Function to process data
   const processData = (data) => {
-    // Find the mosquito with the highest detected count
     const mostFrequentMosquito = data.reduce((max, mosquito) => 
       mosquito.detected_count > max.detected_count ? mosquito : max, data[0]);
 
-    // Filter out the most frequent mosquito from the rest
     const remainingMosquitoes = data
       .filter(mosquito => mosquito.mosquito_name !== mostFrequentMosquito.mosquito_name)
       .sort((a, b) => b.detected_count - a.detected_count);
@@ -103,27 +96,25 @@ const Homepage = () => {
     return <p>Loading data...</p>;
   }
 
-  // Find the mosquito with the highest count
   const totalDetected = mosquitoData.reduce((sum, mosquito) => sum + mosquito.detected_count, 0);
-  // const otherMosquitoes = mosquitoData.stats.filter(m => m.species_name !== mostDetected.species_name);
 
   return (
-    <div className='w-full min-h-screen bg-slate-200'>
-      <div className='py-12'>
+    <div className='w-screen min-h-screen bg-slate-200'>
+      <div className='py-8'>
         <h1 className='text-center text-5xl font-bold text-blue-500'>MOSMO</h1>
         <p className='text-center font-light italic'>Mosquito Monitoring</p>
       </div>
 
-      <div className='flex items-start justify-start gap-5 p-8'>
-        <div className='flex-1 p-5 shadow-xl rounded-md'>
+      <div className='flex flex-wrap items-start justify-start gap-5 p-px md:p-8'>
+        <div className='border border-slate-500 min-w-80 md:flex-1 p-5 shadow-md rounded-md'>
           <p className='text-2xl font-bold text-slate-700'>Area is prone to:</p>
-          <div className='flex justify-start items-start gap-12 mt-6'>
-            <img className='w-56' src={mosquitoImages[mostFrequent.mosquito_name]} alt='' />
+          <div className='flex flex-wrap justify-start items-start mt-6'>
+            <img className='w-56 mx-auto md:mx-px' src={mosquitoImages[mostFrequent.mosquito_name]} alt='' />
             <div className='w-full'>
-              <p className='text-4xl font-semibold text-slate-800'>{mostFrequent.mosquito_name}</p>
-              <p className='indent-8 mt-8'>{mostFrequent.mosquito_description}</p>
+              <p className='text-xl md:text-4xl font-semibold text-slate-800 mt-4'>{mostFrequent.mosquito_name}</p>
+              <p className='indent-8 mt-4'>{mostFrequent.mosquito_description}</p>
 
-              <div className='mt-5 py-6 flex gap-8 items-center justify-start'>
+              <div className='mt-3 py-6 flex gap-8 items-center justify-start'>
                 <div className='border-2 border-slate-600 p-5 text-center rounded-md'>
                   <p className='text-4xl font-semibold'>{mostFrequent.detected_count}</p>
                   <p className='text-sm'>Total Count</p>
@@ -137,10 +128,10 @@ const Homepage = () => {
           </div>
         </div>
 
-        <div className='flex-1 p-5 rounded-md'>
+        <div className='border border-slate-500 flex-1 p-5 rounded-md'>
             <p className='text-2xl font-bold text-slate-700'>Diseases</p>
             {mostFrequent.diseases.split(',').map((disease, index) => (
-                <div key={index} className='flex items-center justify-start gap-5 p-3 shadow2xl mt-5'>
+                <div key={index} className='flex items-center justify-start gap-5 p-3 shadow-md mt-4'>
                   <img className='w-25 rounded-md' src={diseasesImages[disease]} alt="diseasesImages[disease]" />
                   <p>{disease}</p>
                 </div>
@@ -149,13 +140,13 @@ const Homepage = () => {
         </div>
       </div>
 
-      <p className='ps-8 mt-5 text-lg font-bold text-slate-700'>Other Mosquitoes spotted in the area:</p>
-      <div className='mt-5 flex flex-wrap items-stretch justify-start gap-5 p-8'>
+      <p className='ps-8 mt-12 text-lg font-bold text-slate-700'>Other Mosquitoes spotted in the area:</p>
+      <div className='mt-5 flex flex-wrap items-stretch justify-start gap-12 p-8'>
           {otherMosquitoes.map((mosquito, index) => (
-              <div key={index} className='flex flex-col gap-4 p-4 shadow-xl'>
+              <div key={index} className='border border-slate-500 rounded-md flex flex-col p-4 shadow-md'>
                 <img className='w-80 h-52 rounded-md mx-auto' src={mosquitoImages[mosquito.mosquito_name]} alt={mosquito.mosquito_name} />
-                <p className='text-pretty text-xl text-slate-800 font-bold'>{mosquito.mosquito_name}</p>
-                <p className='indent-4 max-w-80'>{mosquito.mosquito_description}</p>
+                <p className='text-pretty text-xl text-slate-800 font-bold mt-4'>{mosquito.mosquito_name}</p>
+                <p className='indent-4 max-w-80 mt-4'>{mosquito.mosquito_description}</p>
     
                 <div className='py-6 flex gap-8 items-center justify-start mt-auto'>
                   <div className='border-2 border-slate-600 p-5 text-center rounded-md'>
